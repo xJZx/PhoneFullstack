d1dc0d99edf026648fe349406d96b7fed4d2204c
xJZx <jakubsuski10@gmail.com>
2023-08-28 00:20:14 +0200

    ninth version (creating different htmls for registration and logging in, changes prior to creating login.html etc.)

--- src/main/java/com/example/phonefullstack/controller/RegistrationController.java
+++ src/main/java/com/example/phonefullstack/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.example.phonefullstack.controller;
 
 import com.example.phonefullstack.model.User;
+import com.example.phonefullstack.repository.UserRepository;
 import com.example.phonefullstack.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,24 +9,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class UserController {
+public class RegistrationController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/user")
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/registration")
     public String user(Model model){
         model.addAttribute("user", new User());
-        return "user";
+        return "registration";
     }
 
     @PostMapping("/register")
-    public String login(@ModelAttribute User user, Model model){
+    public String register(@ModelAttribute User user, Model model){
+//        checking if the user already exist in the DB
+        if (userRepository.findById(user.getUser_id()).isPresent()){
+            return "registration";
+        }
 //        saveUser() - saving to DB
         userService.saveUser(user);
         System.out.println(user.toString());
-        return "index";
+        return "login";
     }
 }
