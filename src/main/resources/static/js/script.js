a8ca78f8b98eb055f2d207a24288ad5abf649aaf
xJZx <jakubsuski10@gmail.com>
2023-09-06 22:16:36 +0200

    twelve version (higlighting currently selected navlink, fixed script.js, needed to alter the htmls and css for this update)

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,8 +5,10 @@ navLinkEls.forEach(navLinkEl => {
     const navLinkPathname = new URL(navLinkEl.href).pathname;
 
     if(windowPathname === navLinkPathname){
+        // navLinkEl.classList.remove("menu li a");
         navLinkEl.classList.add('active');
+        // navLinkEl.classList.replace("menu li a", 'active');
     }
 });
 
-document.getElementById()
+// document.getElementById()
